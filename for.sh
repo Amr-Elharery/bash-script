319c5b202d42534454ab510ecf567aa9d1c37be4
Amr-Elharery <amrelharery@gmail.com>
2024-05-04 10:28:50 -0400

    Update for loop script

--- for.sh
+++ for.sh
@@ -1,3 +1,4 @@
+echo "Files:"
 for item in *
 do
 	if [ -f $item ]
@@ -8,4 +9,25 @@ do
 		echo $item DIR
 	
 	fi
+done
+
+echo "C-like for loop"
+for ((i = 0 ; i < 10 ; i++))
+do
+ 	echo "$i"
+done
+
+
+echo "Range from 1-5"
+for i in {1..5}
+do
+    	echo "Welcome $i"
+done
+
+
+
+echo "Range but With step size"
+for i in {5..50..5}
+do
+  	echo "Welcome $i"
 done
